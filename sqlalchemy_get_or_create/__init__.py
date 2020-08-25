d025d2bb973dabff977ff0ab78f7f70f658be4ab
Enrico Barzetti <enricobarzetti@gmail.com>
2020-08-25 11:03:00 -0700

    readme

--- sqlalchemy_get_or_create/__init__.py
+++ sqlalchemy_get_or_create/__init__.py
@@ -1,7 +1,7 @@
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm.exc import NoResultFound
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 
 def _extract_model_params(defaults, **kwargs):
