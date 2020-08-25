0c36f07f731b7f6772e09799fb25d7937181d172
Enrico Barzetti <enricobarzetti@gmail.com>
2020-08-25 10:46:35 -0700

    first version

---
+++ README.rst
@@ -0,0 +1,32 @@
+SQLAlchemy get_or_create()
+==========================
+
+SQLAlchemy versions of Django's get_or_create() and update_or_create()
+
+Installation
+------------
+
+To get the latest stable release from PyPi
+
+.. code-block:: bash
+
+    pip install sqlalchemy_get_or_create
+
+Usage
+-----
+
+get_or_create(session, model, defaults=None, **kwargs)
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Same as Django's `get_or_create()` but also takes the SQLAlchemy session and model
+
+update_or_create(session, model, defaults=None, **kwargs)
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Same as Django's `update_or_create()` but also takes the SQLAlchemy session and model
+
+Acknowledgments
+===============
+
+1. Django
+1. Some code cribbed from https://skien.cc/blog/2014/01/15/sqlalchemy-and-race-conditions-implementing-get_one_or_create/
---
+++ sqlalchemy_get_or_create/__init__.py
@@ -0,0 +1,61 @@
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm.exc import NoResultFound
+
+__version__ = '0.1.0'
+
+
+def _extract_model_params(defaults, **kwargs):
+    defaults = defaults or {}
+    ret = {}
+    ret.update(kwargs)
+    ret.update(defaults)
+    return ret
+
+
+def _create_object_from_params(session, model, lookup, params, lock=False):
+    obj = model(**params)
+    session.add(obj)
+    try:
+        with session.begin_nested():
+            session.flush()
+    except IntegrityError:
+        session.rollback()
+        query = session.query(model).filter_by(**lookup)
+        if lock:
+            query = query.with_for_update()
+        try:
+            obj = query.one()
+        except NoResultFound:
+            raise
+        else:
+            return obj, False
+    else:
+        return obj, True
+
+
+def get_or_create(session,
+                  model,
+                  defaults=None,
+                  **kwargs):
+    try:
+        return session.query(model).filter_by(**kwargs).one(), False
+    except NoResultFound:
+        params = _extract_model_params(defaults, **kwargs)
+        return _create_object_from_params(session, model, kwargs, params)
+
+
+def update_or_create(session, model, defaults=None, **kwargs):
+    defaults = defaults or {}
+    with session.begin_nested():
+        try:
+            obj = session.query(model).with_for_update().filter_by(**kwargs).one()
+        except NoResultFound:
+            params = _extract_model_params(defaults, **kwargs)
+            obj, created = _create_object_from_params(session, model, kwargs, params, lock=True)
+            if created:
+                return obj, created
+        for k, v in defaults.items():
+            setattr(obj, k, v)
+        session.add(obj)
+        session.flush()
+    return obj, False
